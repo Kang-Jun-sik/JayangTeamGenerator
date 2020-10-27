3d8a41bad25d3749f6eba255caa631e6ffe2ea1a
강준식 <kjs1436@douzone.com>
2020-10-27 10:32:55 +0900

    - 초기 개발 커밋 2

---
+++ src/components/MainWrapper.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="flex-container">
+    <div>
+      <router-link :to="{path:'/ongamelistview',query:{playercount:6}}">3:3</router-link>
+    </div>
+    <div>
+      <router-link :to="{path:'/ongamelistview',query:{playercount:8}}">4:4</router-link>
+    </div>
+    <div>
+      <router-link :to="{path:'/ongamelistview',query:{playercount:10}}">5:5</router-link>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MainWrapper",
+  methods : {
+    goto3on3game : function (e){
+      e.preventDefault();
+    }
+  }
+}
+</script>
+
+<style scoped>
+.flex-container {
+  display: flex;
+  flex-flow: row wrap;
+  justify-content: center;
+  align-content: space-around;
+  width: 80%;
+  background-color: black;
+}
+
+.flex-container>div {
+  background-color: DodgerBlue;
+  color: white;
+  width: 100px;
+  margin: 10px;
+  flex: 0 1 25%;
+  text-align: center;
+  line-height: 75px;
+  font-size: 30px;
+}
+
+/* Responsive layout - makes the two columns stack on top of each other instead of next to each other */
+@media (max-width: 2500px) {
+  .flex-container>div {
+    max-width: 100%;
+    flex: 0 1 100%;
+  }
+}
+</style>
---
+++ src/views/ongamelistview.vue
@@ -0,0 +1,151 @@
+<template>
+  <div class="ongamelistview">
+    <!--<h1>{{ $route.query.playercount }}</h1> -->
+
+    <div class="teamMakeRandomBtn">랜덤팀 짜기</div>
+    <div class="teamMakeRandomBtn">포지션 고려</div>
+    <button class="guestplusBtn">+게스트추가</button>
+    <table class="table table-striped table-hover">
+      <thead>
+      <tr>
+        <th>
+          <!--
+          <label class="form-checkbox">
+            <input type="checkbox" v-model="selectAll" @click="select">
+            <i class="form-icon"></i>
+          </label>
+          -->
+        </th>
+        <th>멤버</th>
+        <th>포지션</th>
+      </tr>
+      </thead>
+      <tbody>
+
+      <tr v-for="i in items" v-bind:key="i.name">
+        <td>
+          <label class="form-checkbox">
+            <input type="checkbox" :value="i.name" v-model="selected" @click="selectedcheck($event)">
+            <i class="form-icon"></i>
+          </label>
+        </td>
+        <td>{{ i.name }}</td>
+        <td>{{ i.position }}</td>
+      </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+//link https://codepen.io/nikitamarcius/pen/LQOaxd
+export default {
+  name: "ongamelistview",
+  data: () => ({
+    items: [
+      {
+        "name": "김지성",
+        "position": 3
+      },
+      {
+        "name": "강준식",
+        "position": 1
+      },
+      {
+        "name": "조성원",
+        "position": 2
+      },
+      {
+        "name": "이승한",
+        "position": 3
+      },
+      {
+        "name": "안연수",
+        "position": 1
+      },
+      {
+        "name": "오세혁",
+        "position": 2
+      },
+      {
+        "name": "정석재",
+        "position": 4
+      },
+      {
+        "name": "최종호",
+        "position": 4
+      },
+      {
+        "name": "장우식",
+        "position": 2
+      },
+      {
+        "name": "조재희",
+        "position": 1
+      },
+      {
+        "name": "김성태",
+        "position": 2
+      },
+      {
+        "name": "안환석",
+        "position": 4
+      },
+      {
+        "name": "이정훈",
+        "position": 3
+      },
+      {
+        "name": "최정훈",
+        "position": 3
+      },
+      {
+        "name": "어웅선",
+        "position": 2
+      },
+      {
+        "name": "유호연",
+        "position": 4
+      },
+      {
+        "name": "이준형",
+        "position": 3
+      }
+    ],
+    selected: [],
+    selectAll: false
+  }),
+  methods: {
+    selectedcheck(e) {
+      console.log(e);
+    },
+
+    select() {
+      this.selected = [];
+      if (!this.selectAll) {
+        for (let i in this.items) {
+          this.selected.push(this.items[i].name);
+        }
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+.teamMakeRandomBtn{
+  background: pink;
+  width: 130px;
+  height: 30px;
+  border: dotted;
+  text-align: center;
+  float: right;
+}
+.guestplusBtn{
+  width: 130px;
+  height: 30px;
+  border: dotted;
+  float: right;
+  background: dodgerblue;
+}
+</style>
